158265bfa689a04d6a4ccff0d0f199212582c55c
Nicolas199920 <65955081+Nicolas199920@users.noreply.github.com>
2020-06-09 15:24:47 +0200

    Update main.c

--- Programm/main.c
+++ Programm/main.c
@@ -30,7 +30,7 @@ printf("Ende:");
 scanf("%d", &ende);
 
 
-FILE *thefile = fopen("data.csv", "r");
+FILE *thefile = fopen("Test_Data_20k_IMU-LSM9DS1.csv", "r");
     if (NULL==thefile)
     {
         printf("Fehler");
@@ -41,11 +41,14 @@ FILE *thefile = fopen("data.csv", "r");
 
 while (EOF!=fscanf(thefile,"%d;%x;%x;%x;%x;%x;%x;%x;%x;%x", &zahl1, &zahl2, &zahl3, &zahl4, &zahl5, &zahl6, &zahl7, &zahl8, &zahl9, &zahl10)) {
 
-//zahl3 = Euklid(zahl1, zahl2);
-if(zahl1>=anfang&& zahl1<=ende){
-printf("Zeitpunkt: %d Sek\nAccelerometer: |%08d X-Achse| |%08d Y-Achse| %08d Z-Achse|\nGyroskop:      |%08d X-Achse| |%08d Y-Achse| %08d Z-Achse|\nMagnetometer:  |%08d X-Achse| |%08d Y-Achse| %08d Z-Achse|\n\n", zahl1, zahl2, zahl3, zahl4, zahl5, zahl6, zahl7, zahl8, zahl9, zahl10 );
-}
 
+if(zahl1>=anfang&& zahl1<=ende){
+
+        //if(zahl2 & 0x80000000){
+
+printf("Zeitpunkt: %d Sek\nAccelerometer: |%8d X-Achse| |%8d Y-Achse| %8d Z-Achse|\nGyroskop:      |%8d X-Achse| |%8d Y-Achse| %8d Z-Achse|\nMagnetometer:  |%8d X-Achse| |%8d Y-Achse| %8d Z-Achse|\n\n", zahl1, zahl2-1, zahl3-1, zahl4-1, zahl5-1, zahl6-1, zahl7-1, zahl8-1, zahl9-1, zahl10-1 );
+}
+//}
 }
 fclose(thefile);
 
