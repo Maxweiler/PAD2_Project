ffdea7bda8a05585159612e07bf3da6643ae0833
Nicolas199920 <65955081+Nicolas199920@users.noreply.github.com>
2020-06-10 13:44:12 +0200

    .

--- Programm/functions.c
+++ Programm/functions.c
@@ -22,15 +22,17 @@ void readLSM(LSM test){
     while(EOF!=fscanf(thefile,"%d;%x;%x;%x;%x;%x;%x;%x;%x;%x",&test.tstamp,&test.accx,&test.accy,&test.accz,&test.gyrox,&test.gyroy,&test.gyroz,&test.magx,&test.magy,&test.magz)) {
 
 
-        if(test.tstamp>=anfang&& test.tstamp<=ende){
+        if(test.tstamp>=anfang&& test.tstamp<=ende)
+            {
 
-        if(test.accx & 2147483648){
 
-             test.accx=test.accx-4294967295;
-}
+            if(test.accx & 2147483648)
+                {
+                test.accx=test.accx-4294967295;
+                }
             printf("Zeitpunkt: %d Sek\nAccelerometer   [mg/LSB]:|%5d X-Achse| |%5d Y-Achse| %5d Z-Achse|\nGyroskop      [mdps/LSB]:|%5d X-Achse| |%5d Y-Achse| %5d Z-Achse|\nMagnetometer[mgauss/LSB]:|%5d X-Achse| |%5d Y-Achse| %5d Z-Achse|\n\n", test.tstamp,test.accx,test.accy,test.accz,test.gyrox,test.gyroy,test.gyroz,test.magx,test.magy,test.magz);
 
-        }
+            }
     }
     fclose(thefile);
 
