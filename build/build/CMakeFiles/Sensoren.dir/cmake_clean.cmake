328f4f7e979d0f4c5bbbb2ab530f2a3da3e541e1
Maurice96 <64415381+Maurice96@users.noreply.github.com>
2020-06-12 23:28:26 +0200

    Ordner Struktur

---
+++ build/build/CMakeFiles/Sensoren.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../bin/Sensoren.exe"
+  "../bin/Sensoren.exe.manifest"
+  "../bin/Sensoren.pdb"
+  "../lib/libSensoren.dll.a"
+  "CMakeFiles/Sensoren.dir/main.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Sensoren.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/build/CMakeFiles/functions.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../lib/libfunctions.a"
+  "../lib/libfunctions.pdb"
+  "CMakeFiles/functions.dir/functions.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/functions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
